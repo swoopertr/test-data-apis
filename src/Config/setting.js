eda7318b6366c6fc9b9f3129f399f586cb439212
swoopertr <tunc.kiral@gmail.com>
2023-09-12 08:47:51 +0300

    .

--- src/Config/setting.js
+++ src/Config/setting.js
@@ -22,7 +22,7 @@ settings.virtualRootPath = '/virt/';
 
 settings.controllerFolder = './Controller/';
 settings.jsonPath = "/src/Config/Routes.json";
-settings.ServerPort = process.env.PORT || 8082;
+settings.ServerPort = process.env.PORT || 8083;
 
 settings.errorController = './Controller/error';
 
